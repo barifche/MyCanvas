bfdb6a186a3948a69fa64aab8c5a4d58e3ffc9d4
barifche <rifbache@gmail.com>
2020-04-26 08:24:02 +0200

    Config  de faire tourner mon serpent

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ window.onload = function ()
         canvas.style.border = "1px solid";
         this.document.body.appendChild(canvas);
         ctx = canvas.getContext ('2d');
-        snakee = new Snake([[6,4],[5,4],[4,4]]);
+        snakee = new Snake([[6,4],[5,4],[4,4]] , "right");
         refreshCanvas();
 
     }
@@ -39,9 +39,10 @@ window.onload = function ()
         var y = position[1] * blockSize;
         ctx.fillRect(x, y, blockSize, blockSize);
     }
-    function Snake(body)
+    function Snake(body , direction)
     {
         this.body = body;
+        this.direction = direction;
         this.draw = function()
         {
             ctx.save();
@@ -57,10 +58,71 @@ window.onload = function ()
         this.advance = function()
         {
             var nextPosition = this.body[0].slice();
-            nextPosition[0] += 1;
+            switch(this.direction)
+            {
+                case "left":
+                    nextPosition[0] -= 1;
+                    break;
+                case "right":
+                    nextPosition[0] += 1;
+                    break;
+                case "down":
+                    nextPosition[0] += 1;
+                    break;
+                case "up":
+                    nextPosition[0] -= 1;
+                    break;
+                default:
+                    throw("Invalid Direction");
+            }
+            
             this.body.unshift(nextPosition);
             this.body.pop();
         };
+        this.setDirection = function(newDirections)
+        {
+            var allowedDirection;
+            switch(this.direction)
+            {
+                case "left":
+                case "right":
+                    allowedDirection = ["up" , "down"];
+                    break;
+                case "up":
+                case "down":
+                    allowedDirection = ["left" , "right"];
+                    break;
+                default:
+                        throw("Invalid Direction");
+            }
+            if (allowedDirection.indexOf(newDirection) > -1)
+            {
+              this.direction = newDirection;
+            }
+        };
 
+    }
+    this.document.onkeydown = function handlekeydown(e)
+    {
+        var key = e.keyCode;
+        var newDirection;
+        switch(key)
+        {
+            case 37:
+                newDirection = "left";
+                break;
+            case 38:
+                newDirection = "up";
+                break; 
+            case 39:
+                newDirection = "right";
+                break; 
+            case 40:
+                newDirection = "down";
+                break;  
+            default:
+                    return;  
+        }
+        snakee.setDirection(newDirection);
     }
 }
